38d3297874186b443f0d09c7dc0caebe49497364
ATE <ATE@DESKTOP-ODIPDNU>
2022-07-13 13:54:28 +0900

    ** 주석 추가

--- src/main/java/com/mirim/test/TestController.java
+++ src/main/java/com/mirim/test/TestController.java
@@ -12,7 +12,7 @@ public class TestController {
 	@RequestMapping(value = "/write")   //value는 잡아올 요청값, views에서 jsp파일 만들어줘야함
 	public String write(Model model) {			// value값과 일치하지 않아도 됨, model 객체를 통해 view를 전달
 		
-		model.addAttribute("id", "tiger");   // (name, value)
+		model.addAttribute("id", "tiger");   // addAttribute(name, value), 데이터 전달
 		
 		
 		return "write";			// view의 이름 -> jsp파일의 이름
